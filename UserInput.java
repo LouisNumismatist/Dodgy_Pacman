36bff49a5bc110dffda2d0986cb88c2838ce6ce6
Louis_Broughton <l.broughton@lancaster.ac.uk>
2020-02-07 14:10:40 +0000

    Create UserInput.java
    Created and setup UserInput class

---
+++ UserInput.java
@@ -0,0 +1,22 @@
+public class UserInput {
+
+    
+
+    public static void main() {
+
+        
+    }
+
+    public boolean isKeyClicked() {
+        return true;
+    }
+
+    public boolean isKeyHeld() {
+        return true;
+    }
+
+    public void getKeyboardState() {
+
+    }
+
+}
